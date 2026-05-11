233451b87d22c2c435dc19d9197aed9cd2477a16
Kyle <sadoe3@gmail.com>
2026-05-11 17:47:20 +0900

    chnage version info

--- src/server/HtmlTemplates.hpp
+++ src/server/HtmlTemplates.hpp
@@ -54,6 +54,6 @@ button:hover{background:#005a9e;}
 
     <button type="submit">Save Configuration</button>
 </form>
-<div class="footer">HIRA Helper Solution v3.1</div>
+<div class="footer">HIRA Helper Solution v3.2</div>
 </body></html>
 )";
